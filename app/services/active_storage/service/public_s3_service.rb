af7012c47bd4e2c0805b39de500cc496f1bc88d4
César Gracián <cesar.gracian@icloud.com>
2025-09-12 17:05:56 -0600

    Refactor PublicS3Service to improve URL generation methods and enhance code clarity

--- app/services/active_storage/service/public_s3_service.rb
+++ app/services/active_storage/service/public_s3_service.rb
@@ -1,9 +1,14 @@
 require "active_storage/service/s3_service"
 
 class ActiveStorage::Service::PublicS3Service < ActiveStorage::Service::S3Service
-  # Usar URL directa en lugar de firmada
-  def url(key, **)
-    File.join(@client.config.endpoint.to_s, @bucket.name, key)
+  # URL directa (sin firma)
+  def url(key, **_options)
+    "#{@client.config.endpoint}/#{@bucket.name}/#{key}"
+  end
+
+  # Para representaciones (miniaturas)
+  def url_for_direct_upload(key, **_options)
+    url(key)
   end
 
   def public?
