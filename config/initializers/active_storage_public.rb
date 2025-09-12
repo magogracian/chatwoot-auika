44ca145bddfcb168e57ace9cefd84df7ecf61622
César Gracián <cesar.gracian@icloud.com>
2025-09-12 16:25:08 -0600

    Remove ActiveStorage configuration and load PublicS3Service on initialization

--- config/initializers/active_storage_public.rb
+++ config/initializers/active_storage_public.rb
@@ -1,11 +1,2 @@
-Rails.application.config.after_initialize do
-  ActiveStorage::Service.url_expires_in = 10.years # casi infinito, opcional
-
-  if ActiveStorage::Blob.service.name.to_s == "s3_compatible"
-    ActiveStorage::Blob.service.singleton_class.class_eval do
-      def public?
-        true
-      end
-    end
-  end
-end
+# Cargar el servicio personalizado PublicS3Service al arrancar
+require Rails.root.join("app/services/active_storage/service/public_s3_service.rb")
