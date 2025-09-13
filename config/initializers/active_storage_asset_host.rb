5eb520319f30485182b436eea6b8de00ee81e922
César Gracián <cesar.gracian@icloud.com>
2025-09-12 22:25:43 -0600

    Remove ActiveStorage asset host initializer

--- config/initializers/active_storage_asset_host.rb
+++
@@ -1,8 +0,0 @@
-if ENV['STORAGE_ASSET_HOST'].present?
-  Rails.application.config.after_initialize do
-    host = ENV['STORAGE_ASSET_HOST']
-    Rails.application.config.active_storage.asset_host = host
-    Rails.application.config.action_controller.asset_host = host
-    Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_redirect
-  end
-end
