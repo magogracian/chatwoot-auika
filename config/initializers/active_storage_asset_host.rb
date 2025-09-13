9153ab5f429d17dbacfebb46c31ee11a61f55fe1
César Gracián <cesar.gracian@icloud.com>
2025-09-12 22:11:19 -0600

    Refactor ActiveStorage asset host configuration initializer for clarity

--- config/initializers/active_storage_asset_host.rb
+++ config/initializers/active_storage_asset_host.rb
@@ -1,6 +1,8 @@
 if ENV['STORAGE_ASSET_HOST'].present?
   Rails.application.config.after_initialize do
-    Rails.application.config.active_storage.asset_host = ENV['STORAGE_ASSET_HOST']
+    host = ENV['STORAGE_ASSET_HOST']
+    Rails.application.config.active_storage.asset_host = host
+    Rails.application.config.action_controller.asset_host = host
     Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_redirect
   end
 end
